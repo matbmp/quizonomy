78c52315614eeb44b62c5fc32702c274cd498cd8
matbmp <96355280+matbmp@users.noreply.github.com>
2023-05-11 15:55:51 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+
+<script>
+    import {Button} from 'flowbite-svelte'
+</script>
+
+<div class='p-4 text-lg'>
+    <h1 class='text-2xl'>Welcome to SvelteKit</h1>
+    This template includes:
+    <ul>
+        <li class='my-2'><button class='rounded-xl bg-blue-400 text-white p-2 px-4'>Tailwind</button></li>
+        <li class='my-2'><Button color='red'>Flowbite</Button></li>
+    </ul>
+    
+</div>
